868555f601615789a0697ef4de3067e29ed2eaac
PPRonee <100339149+PPRonee@users.noreply.github.com>
2023-02-14 18:08:07 +0100

    maj

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,14 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
+  env: { node: true, jest: true },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
